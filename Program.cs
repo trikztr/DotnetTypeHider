772062833da4e0d2b7734237fa6e5b443b0f5013
Trym Henriksen Zimmer <trym.zimmer@draga.no>
2026-01-01 01:31:08 +0100

    chore: initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AsmResolver.DotNet;
+using AsmResolver.DotNet.Signatures;
+
+Console.WriteLine("Hello, World!");
+
+var module = ModuleDefinition.FromFile(args[0]);
+var factory = module.CorLibTypeFactory;
+var compilerGeneratedAttribute = factory
+    .CorLibScope.CreateTypeReference(
+        "System.Runtime.CompilerServices",
+        "CompilerGeneratedAttribute"
+    )
+    .CreateMemberReference(".ctor", MethodSignature.CreateInstance(factory.Void))
+    .ImportWith(module.DefaultImporter);
+foreach (var type in module.GetAllTypes())
+{
+    if (!type.IsClass || type.IsModuleType || type.IsCompilerGenerated())
+    {
+        continue;
+    }
+    type.CustomAttributes.Add(new CustomAttribute(compilerGeneratedAttribute));
+    type.Name = $"<>AnonType_{type.MetadataToken}";
+}
+module.Write("out.dll");
---
+++ README.md
@@ -0,0 +1,6 @@
+# DotnetTypeHider
+
+Hides classes from some decompilers by setting CompilerGenerate flag and changing name to resemble an anonymous type.
+Heavily inspired by <https://github.com/Washi1337/AwaitFuscator>
+![Input Program](input.png)
+![Output Program](output.png)
