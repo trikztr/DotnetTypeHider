781dbf407320cc5bb0655a726f69818511fbf278
Trym Henriksen Zimmer <trym.zimmer@draga.no>
2026-01-01 09:37:15 +0100

    feat: add call hiding

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-using AsmResolver.DotNet;
+﻿using AsmResolver.DotNet;
+using AsmResolver.DotNet.Code.Cil;
 using AsmResolver.DotNet.Signatures;
-
-Console.WriteLine("Hello, World!");
+using AsmResolver.PE.DotNet.Cil;
+using AsmResolver.PE.DotNet.Metadata.Tables.Rows;
 
 var module = ModuleDefinition.FromFile(args[0]);
 var factory = module.CorLibTypeFactory;
+
+// CompilerGeneratedAttribute ctor
 var compilerGeneratedAttribute = factory
     .CorLibScope.CreateTypeReference(
         "System.Runtime.CompilerServices",
@@ -13,13 +15,108 @@ var compilerGeneratedAttribute = factory
     )
     .CreateMemberReference(".ctor", MethodSignature.CreateInstance(factory.Void))
     .ImportWith(module.DefaultImporter);
+
+// Rename + mark types
 foreach (var type in module.GetAllTypes())
 {
-    if (!type.IsClass || type.IsModuleType || type.IsCompilerGenerated())
-    {
+    if (type.IsValueType || !type.IsClass || type.IsModuleType || type.IsCompilerGenerated())
         continue;
-    }
+
     type.CustomAttributes.Add(new CustomAttribute(compilerGeneratedAttribute));
     type.Name = $"<>AnonType_{type.MetadataToken}";
 }
+
+// Hide calls in anonymous types constructors
+foreach (var type in module.GetAllTypes())
+{
+    foreach (var method in type.Methods)
+    {
+        if (method.CilMethodBody is not { } body)
+            continue;
+
+        var instructions = body.Instructions;
+
+        for (int i = 0; i < instructions.Count; i++)
+        {
+            var instruction = instructions[i];
+
+            if (
+                (instruction.OpCode != CilOpCodes.Call && instruction.OpCode != CilOpCodes.Callvirt)
+                || instruction.Operand
+                    is not IMethodDescriptor
+                    {
+                        Signature: { ReturnsValue: false, HasThis: false }
+                    } targetDescriptor
+                || targetDescriptor.Resolve() is not { } target
+            )
+                continue;
+
+            var anonymousType = CreateAnonymousTypeForCall(module, target);
+            var anonymousTypeConstructor = anonymousType.Methods.Single(m => m.Name == ".ctor");
+
+            // Replace call with newobj
+            instruction.OpCode = CilOpCodes.Newobj;
+            instruction.Operand = module.DefaultImporter.ImportMethod(anonymousTypeConstructor);
+
+            // Discard created object
+            instructions.Insert(i + 1, CilOpCodes.Pop);
+            i++;
+        }
+    }
+}
+
 module.Write("out.dll");
+return;
+
+TypeDefinition CreateAnonymousTypeForCall(ModuleDefinition module, MethodDefinition target)
+{
+    var anonymousType = new TypeDefinition(
+        "",
+        $"<>AnonType_{Guid.NewGuid():N}",
+        TypeAttributes.Class | TypeAttributes.NotPublic | TypeAttributes.Sealed,
+        factory.Object.Type
+    );
+    anonymousType.CustomAttributes.Add(new CustomAttribute(compilerGeneratedAttribute));
+
+    var constructorSignature = MethodSignature.CreateInstance(
+        factory.Void,
+        [.. target.Signature!.ParameterTypes]
+    );
+    var constructor = new MethodDefinition(
+        ".ctor",
+        MethodAttributes.Public
+            | MethodAttributes.HideBySig
+            | MethodAttributes.SpecialName
+            | MethodAttributes.RuntimeSpecialName,
+        constructorSignature
+    );
+
+    var body = new CilMethodBody(constructor);
+    var instructions = body.Instructions;
+
+    // object::.ctor
+    var objectConstructor = factory.Object.Type.Resolve()!.GetConstructor()!;
+
+    instructions.Add(CilOpCodes.Ldarg_0);
+    instructions.Add(CilOpCodes.Call, module.DefaultImporter.ImportMethod(objectConstructor));
+
+    // forward arguments
+    for (int i = 0; i < constructor.Parameters.Count; i++)
+    {
+        instructions.Add(CilOpCodes.Ldarg, constructor.Parameters[i]);
+    }
+
+    instructions.Add(
+        target.IsVirtual ? CilOpCodes.Callvirt : CilOpCodes.Call,
+        module.DefaultImporter.ImportMethod(target)
+    );
+
+    instructions.Add(CilOpCodes.Ret);
+    instructions.CalculateOffsets();
+
+    constructor.CilMethodBody = body;
+    anonymousType.Methods.Add(constructor);
+    module.TopLevelTypes.Add(anonymousType);
+
+    return anonymousType;
+}
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # DotnetTypeHider
 
 Hides classes from some decompilers by setting CompilerGenerate flag and changing name to resemble an anonymous type.
-Heavily inspired by <https://github.com/Washi1337/AwaitFuscator>
+Hide method calls in anonymous type constructors.
+Inspired by <https://github.com/Washi1337/AwaitFuscator>
 ![Input Program](input.png)
 ![Output Program](output.png)
